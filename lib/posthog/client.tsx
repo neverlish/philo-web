096348c3d4b6625eba79cc08cfd80b0d69b8b6e3
neverlish <neverlish@gmail.com>
2026-03-15 20:37:11 +0900

    feat(posthog): only initialize in production

--- lib/posthog/client.tsx
+++ lib/posthog/client.tsx
@@ -4,7 +4,7 @@
 import posthog from 'posthog-js'
 import { PostHogProvider as PHProvider } from 'posthog-js/react'
 
-if (typeof window !== 'undefined') {
+if (typeof window !== 'undefined' && process.env.NODE_ENV === 'production') {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     defaults: '2026-01-30',
